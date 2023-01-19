6575c0aadd0d9b3565a45bba55b022b78ce64bea
“ADRIENNE <adrienneklee@gmail.com>
2023-01-18 20:55:27 -0500

    join us page added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Doug Ford Sucks')
+console.log('ARE YOU FED UP?')
+
+const btn = document.querySelector('#main-btn')
+console.log(btn)
+
+let bottomOfBtn = btn.offsetTop;
+
+console.log(bottomOfBtn)
+
+function stickyBtn() {
+    if (window.scrollY >= bottomOfBtn) {
+        btn.classList.add('sticky-bottom')
+    } else {
+        btn.classList.remove('sticky-bottom')
+    }
+}
+
+window.addEventListener('scroll', stickyBtn);
