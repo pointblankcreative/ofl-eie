9486760682a70e2699836faa20ab945d42251db6
“ADRIENNE <adrienneklee@gmail.com>
2023-01-19 20:53:33 -0500

    styling added for desktop

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-console.log('ARE YOU FED UP?')
-
 const btn = document.querySelector('#main-btn');
 let bottomOfBtn = btn.offsetTop;
-
-// const moreSections = document.querySelectorAll('.read-more');
+const windowWidth = window.innerWidth
 
 
 function stickyBtn() {
-    if (window.scrollY >= bottomOfBtn) {
+    if (windowWidth >= 767) {
+        return
+    }
+    else if (window.scrollY >= bottomOfBtn) {
         btn.classList.add('sticky-bottom')
     } else {
         btn.classList.remove('sticky-bottom')
     }
 }
-
-// function toggleOpen() {
-//     console.log('hello')
-//     // this.classList.toggle('open');
-// }
-
-// moreSections.forEach(item => item.addEventListener('click', toggleOpen));
-
 window.addEventListener('scroll', stickyBtn);
 
 
