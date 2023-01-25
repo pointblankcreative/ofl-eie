202b9e5a22401e1c80398cfa9704747357120264
“ADRIENNE <adrienneklee@gmail.com>
2023-01-25 12:22:34 -0500

    First round of BH edits made

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 const btn = document.querySelector('#main-btn');
 let bottomOfBtn = btn.offsetTop;
+const btnText = document.getElementsByClassName('btn-join-en')
 const windowWidth = window.innerWidth
 
 const stickyStar = document.querySelector('#stickyStar')
-console.log(stickyStar)
+let bottomOfStar = stickyStar.offsetTop
 
 
 function stickyBtn() {
     if (windowWidth >= 767) {
         return
     }
-    else if (window.scrollY >= bottomOfBtn) {
+    else if (window.scrollY >= bottomOfBtn ) {
         btn.classList.add('sticky-bottom')
+        btnText[0].innerText = 'JOIN THE FIGHT'
     } else {
         btn.classList.remove('sticky-bottom')
     }
 }
 
 function starBtn() {
-    if (window.scrollY >= bottomOfBtn) {
-        stickyStar.style.top = window.scrollY + 100 + 'px'
+    if (window.scrollY >= bottomOfStar) {
+        stickyStar.style.top = window.scrollY + 50 + 'px'
     } else {
-        stickyStar.style.top = bottomOfBtn
+        stickyStar.style.top = bottomOfStar
     }
 }
 window.addEventListener('scroll', stickyBtn);
